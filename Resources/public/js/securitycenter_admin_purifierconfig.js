6f64a38bdc717eba7c1ed098e3eace912e1759c6
Craig Heydenburg <craigh@mac.com>
2015-08-16 18:35:21 -0400

    convert SecurityCenterModule to psr-4

---
+++ Resources/public/js/securitycenter_admin_purifierconfig.js
@@ -0,0 +1,5 @@
+// Copyright Zikula Foundation 2010 - license GNU/LGPLv2.1 (or at your option, any later version).
+
+function toggleWriteability(node, checked) {
+    document.getElementById(node).disabled = checked;
+}
